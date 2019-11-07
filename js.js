45f9cbd20ec2d34d3a73e8a16a0d5241b076e271
Teera Lucksanapiruk <ithrewmyguitar@gmail.com>
2019-11-07 16:31:04 +0700

    WIP

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
-if(lightbox){
 
   $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
   });
-}
+
 
 
 // isPlaying
@@ -29,35 +28,10 @@ if (typeof idx_start === 'undefined') {
 // prepare string for gallery
 for (i = 0; i < pic_count; i++) {
   var idx = i + idx_start - 1;
-  mstring += "<img src='"+ imgPrefix + pad(idx + 1, padsize) + suffix +"' alt='image'/>";
-  if (!(i % 3)) {
-    if (i != 0) {
-      string += "</div>";
-    }
-    string += '<div class="row p-2">';
-  }
-
-  if(lightbox){
-    string += '<div class="col-sm"><a href="' + imgPrefix + pad(idx + 1, padsize) + suffix + '" data-toggle="lightbox" data-viewport="{ selector: body, padding: 0 }"><img src="' + thumbPrefix + pad(idx + 1, padsize) + suffix +
-    '" class="img-thumbnail rounded-sm border-0 p-0"></a></div>';
-  } else {
-    string += '<div class="col-sm"><a herf="#"><img src="' + thumbPrefix + pad(idx + 1, padsize) + suffix + '" class="mthumb img-thumbnail rounded-sm border-0 p-0"  data-index="' + i + '"></a></div>';
-  }
-
-}
-// pad gallery not divisible by 3
-var ip = pic_count % 3;
-if (ip==2){
-  string+='<div class="col-sm"></div>';
-}
-if (ip==1){
-  string+='<div class="col-sm"></div>';
-  string+='<div class="col-sm"></div>';
+    string += '<div class="gallery-item p-1 p-sm-2"><a href="' + imgPrefix + pad(idx + 1, padsize) + suffix + '" data-toggle="lightbox"><img src="' + thumbPrefix + pad(idx + 1, padsize) + suffix +
+    '" class="rounded-sm border-0"></a></div>'
 }
 
-
-
-$(".MagicScroll").append(mstring);
 $(".gallery").append(string + '</div>');
 var firsttime = true;
 $("body").click(function() {
@@ -74,21 +48,16 @@ $("video").mouseenter(function() {
   $("video").removeAttr('controls');
 });
 
-// MAGIC SCROLL OPTIONS
-var MagicScrollOptions = {};
-var MagicID = "";
-MagicScrollOptions = {
-  // autostart: false,
-  step: 1,
-  lazyLoad: false,
-  onReady: function() {
-    console.log('onReady', (MagicID = arguments[0]));
+var $grid = $('.gallery').isotope({
+  itemSelector: '.gallery-item',
+
+  // layout mode options
+  masonry: {
+    columnWidth: '.isotope-size'
   }
-};
-// var g;
-$('.mthumb').click(function() {
-  var idx = this.attributes['data-index'].value;
-  MagicScroll.jump(MagicID, idx);
 });
 
-MagicScroll.start();
+$('img').on('load', function() {
+  $grid.isotope('layout');
+  console.log('layout');
+});
